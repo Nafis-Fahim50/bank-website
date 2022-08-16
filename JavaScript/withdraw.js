4b18f81ed55989b9b6e7284728085d1687843193
Nafis-Fahim <nafisf96@gmail.com>
2022-08-16 15:34:45 +0600

    calculate withdraw and total balance

---
+++ JavaScript/withdraw.js
@@ -0,0 +1,21 @@
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+    withdrawField.value = " ";
+
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawString);
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    // get total balance 
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+})
