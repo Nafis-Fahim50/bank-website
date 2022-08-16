2b353b01b9632574218d9c8da962ebf543ad64ba
Nafis-Fahim <nafisf96@gmail.com>
2022-08-16 14:11:55 +0600

    get user deposit and set deposit total value

--- JavaScript/deposit.js
+++ JavaScript/deposit.js
@@ -1,3 +1,11 @@
 document.getElementById('btn-diposit').addEventListener('click',function(){
-    
+    const depositField = document.getElementById('deposit-field');
+    const depositAmount = depositField.value;
+    depositField.value = "";
+
+    const depositTotalElement = document.getElementById('deposit-total');
+    const depositTotal = depositTotalElement.innerText;
+    depositTotalElement.innerText = depositAmount;
+
+
 })
