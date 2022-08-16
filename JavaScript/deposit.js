4cf9b2f958b257444d7b9cb113b7331d6017dc62
Nafis-Fahim <nafisf96@gmail.com>
2022-08-16 14:56:44 +0600

    update deposit balance

--- JavaScript/deposit.js
+++ JavaScript/deposit.js
@@ -1,11 +1,25 @@
 document.getElementById('btn-diposit').addEventListener('click',function(){
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
     depositField.value = "";
 
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.innerText;
-    depositTotalElement.innerText = depositAmount;
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    depositTotalElement.innerText = newDepositAmount;
+
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+
+    depositTotalElement.innerText = currentDepositTotal;
+
+    // get balance total 
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
 
 
 })
