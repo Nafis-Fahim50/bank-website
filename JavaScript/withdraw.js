59e26b49d2aa6fa31ee22e40f9a19f3032374ab5
Nafis-Fahim <nafisf96@gmail.com>
2022-08-16 15:54:49 +0600

    added some icon and alart

--- JavaScript/withdraw.js
+++ JavaScript/withdraw.js
@@ -4,17 +4,28 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
     withdrawField.value = " ";
 
+   if(isNaN(newWithdrawAmount)){
+    alert("Please provide a valid amount");
+    return;
+   }
+
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawString);
 
-    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    withdrawTotalElement.innerText = currentWithdrawTotal;
 
     // get total balance 
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    if(newWithdrawAmount > previousBalanceTotal){
+        alert("Insufficient balance");
+        return;
+    }
+
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
 
     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
     balanceTotalElement.innerText = newBalanceTotal;
